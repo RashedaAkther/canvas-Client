939e40c28f066ce6fa2b2d2698f9750ad333b25f
RashedaAkther <rashedaakther006@gmail.com>
2023-11-24 03:11:56 +0600

    real-state-project structure created

---
+++ src/FIrebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDW363bXQog0iT_-bKFn2TLK2vDT1HbvfM",
+  authDomain: "real-state-project-60089.firebaseapp.com",
+  projectId: "real-state-project-60089",
+  storageBucket: "real-state-project-60089.appspot.com",
+  messagingSenderId: "263429768155",
+  appId: "1:263429768155:web:6e04e6f71ed9262e535dfe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
