630c97f11538224599217b5cad7347c6a85b91d0
RashedaAkther <rashedaakther006@gmail.com>
2023-11-28 10:22:15 +0600

    admin route set and admin dashboard created

--- src/Hooks/useAdmin.jsx
+++ src/Hooks/useAdmin.jsx
@@ -1,25 +1,205 @@
-// import { useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
-// import useAxiosSecure from "./useAxiosSecure";
-// import Context from "./useContext";
+import useAxiosSecure from "./useAxiosSecure";
+import Context from "./useContext";
 
 
-// const useAdmin = () => {
-//   const { user } = Context();
-//   // const [admin,setAdmin]=useState()
-//   const axiosSecure = useAxiosSecure();
-//   // const AxiosPublic = useAxiosPublic();
-//   console.log(user);
-//   const { data: isAdmin, isLoading: isAdminLoading } = useQuery({
-//     queryKey: ["requestAdmin", user?.email],
+const useAdmin = () => {
+  const { user } = Context();
+  // const [admin,setAdmin]=useState()
+  const axiosSecure = useAxiosSecure();
+  // const AxiosPublic = useAxiosPublic();
+  console.log(user);
+  const { data: isAdmin, isLoading: isAdminLoading } = useQuery({
+    queryKey: ["requestAdmin", user?.email],
 
-//     queryFn: async () => {
-//       const res = await axiosSecure.get(`/admin/${user?.email}`);
-//       console.log("admin use", res.data);
-//       return res.data;
-//     },
-//   });
-//   return [isAdmin, isAdminLoading];
-// };
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/admin/${user?.email}`);
+      console.log("admin use", res.data);
+      return res.data;
+    },
+  });
+  return [isAdmin, isAdminLoading];
+};
 
-// export default useAdmin;
+export default useAdmin;
+
+
+
+
+{/* <div className=" col-span h-[100vh] md:col-end-2 bg-indigo-400 lg:w-fit ">
+        <ul className="menu p-4">
+        
+        
+          {isAdmin?.isAdmin ? (
+            <div>
+              <li>
+                <NavLink to="/dashboard/AdminProfile">
+                  <FaHome></FaHome>
+                  Admin Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/AddItems">
+                  <FaUtensils></FaUtensils>
+                  Add Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/ManageItems">
+                  <FaList></FaList>
+                  Manage Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/Booking">
+                  <FaBook></FaBook>
+                  Manage Booking
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/Users">
+                  <FaUser></FaUser>
+                  All Users
+              
+                  </NavLink>
+                  </li>
+                </div>
+              ) : (
+                (
+                  <div className=" md ">
+                    <li>
+                      <NavLink to="/Dashboard/UserProfile">
+                        <FaHome></FaHome>
+                        My Profile
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/dashboard/reservation">
+                        <FaCalendar></FaCalendar>
+                        Reservation
+                      </NavLink>
+                    </li>
+    
+                    <li className="mb-2">
+                      <NavLink to="/dashboard/donationRequest">
+                        <FaHistory></FaHistory>
+                        My Donation Request
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/Dashboard/CreateDonation">
+                        <FaAd className=" text-xl"></FaAd>
+                        Create Donation Request
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/dashboard/bookings">
+                        <FaList></FaList>
+                        My Bookings
+                      </NavLink>
+                    </li>
+                    <div className="flex items-center p-2 mt-12 space-x-4 justify-self-end">
+                      <img
+                        src={user?.photoURL}
+                        alt=""
+                        className="w-12 h-12 rounded-lg dark:bg-gray-500"
+                      />
+                      <div>
+                        <h2 className="text-lg font-semibold">{user?.name}</h2>
+                        <span className="flex items-center space-x-1">
+                          <Link
+                            to={"UserProfile"}
+                            rel="noopener noreferrer"
+                            className="text-xs hover:underline dark:text-gray-400"
+                          >
+                            View profile
+                          </Link>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                )
+                ||
+                (isAgent?.isAgent && (
+                  <div className=" md ">
+                    <li>
+                      <NavLink to="/Dashboard/UserProfile">
+                        <FaHome></FaHome>
+                        My Profile
+                      </NavLink>
+                    </li>
+    
+                    <li className="mb-2">
+                      <NavLink to="/dashboard/donationRequest">
+                        <FaHistory></FaHistory>
+                        My Donation Request
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/Dashboard/CreateDonation">
+                        <FaAd className=" text-xl"></FaAd>
+                        Create Donation Request
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink to="/dashboard/bookings">
+                        <FaList></FaList>
+                        My Bookings
+                      </NavLink>
+                    </li>
+                    <div className="flex items-center p-2 mt-12 space-x-4 justify-self-end">
+                      <img
+                        src={user?.photoURL}
+                        alt=""
+                        className="w-12 h-12 rounded-lg dark:bg-gray-500"
+                      />
+                      <div>
+                        <h2 className="text-lg font-semibold">{user?.name}</h2>
+                        <span className="flex items-center space-x-1">
+                          <Link
+                            to={"UserProfile"}
+                            rel="noopener noreferrer"
+                            className="text-xs hover:underline dark:text-gray-400"
+                          >
+                            View profile
+                          </Link>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                ))
+              )}
+              <div className="divider"></div>
+              <li>
+                <NavLink to="/">
+                  <FaHome></FaHome>
+                  Home
+                </NavLink>
+              </li>
+              {isAdmin?.isAdmin || (
+                <li onClick={logOut} className="rounded-sm">
+                  <a
+                    rel="noopener noreferrer"
+                    className="flex items-center p-2 space-x-3 rounded-md"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 512 512"
+                      className="w-5 h-5 fill-current dark:text-gray-400"
+                    >
+                      <path d="M440,424V88H352V13.005L88,58.522V424H16v32h86.9L352,490.358V120h56V456h88V424ZM320,453.642,120,426.056V85.478L320,51Z"></path>
+                      <rect width="32" height="64" x="256" y="232"></rect>
+                    </svg>
+                    <span>Logout</span>
+                  </a>
+                </li>
+              )}
+              <li>
+                <NavLink to="/order/contact">
+                  <FaVoicemail></FaVoicemail>
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div> */}
--- src/Pages/Dashboard/Admin/AdminHome.jsx
+++ src/Pages/Dashboard/Admin/AdminHome.jsx
@@ -1,10 +1,291 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { FaBook, FaDollarSign, FaUsers } from "react-icons/fa";
+
+import { TypeAnimation } from "react-type-animation";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  PieChart,
+  Pie,
+  Legend,
+} from "recharts";
+
+import { Link } from "react-router-dom";
+import Context from "../../../Hooks/useContext";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
 const AdminHome = () => {
+  const { user } = Context();
+  const axiosSecure = useAxiosSecure()
+
+  const { data: stats = {} } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-status");
+      return res.data;
+    },
+  });
+  // BarChart
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+  // PieChart
+  const datas = [
+    { name: "Group A", value: 400 },
+    { name: "Group B", value: 300 },
+    { name: "Group C", value: 300 },
+    { name: "Group D", value: 200 },
+  ];
+
+  // const { data: chartData = [] } = useQuery({
+  //     queryKey: ['order-stats'],
+  //     queryFn: async () => {
+  //         const res = await axiosSecure.get('/order-stats');
+  //         return res.data;
+  //     }
+  // })
+
+  // custom shape for the bar chart
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${
+      x + width / 2
+    },${y + height / 3}
+        ${x + width / 2}, ${y}
+        C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${
+      y + height
+    } ${x + width}, ${y + height}
+        Z`;
+  };
+
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
+
+  // // custom shape for the pie chart
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
     return (
-        <div>
-            <p></p>
-        </div>
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
     );
+  };
+
+  // const pieChartData = chartData.map(data => {
+  //     return {name: data.category, value: data.revenue}
+  // })
+
+  return (
+    <div>
+      <h2 className="text-3xl">
+        <span>Hi, Welcome </span>
+        <TypeAnimation
+          sequence={[
+            // 'arman',
+            user?.displayName ? user.displayName : "Back",
+            1000
+        ]}
+          speed={1}
+          repeat={Infinity}
+          style={{ fontSize: "1em" }}
+        />
+      </h2>
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaDollarSign className="text-3xl"></FaDollarSign>
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">${stats.revenue}</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+
+        <Link to={'/Dashboard/users'} className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers className="text-3xl"></FaUsers>
+          </div>
+          <div className="stat-title">Users</div>
+          <div className="stat-value">{stats.users}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </Link>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaBook className="text-3xl"></FaBook>
+          </div>
+          <div className="stat-title">Menu Items</div>
+          <div className="stat-value">{stats.menusItems}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-8 h-8 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stats.orders}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
+      {/* <div className="flex">
+                <div className="w-1/2">
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={chartData}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="category" />
+                        <YAxis />
+                        <Bar dataKey="quantity" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                            {chartData.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[index % 6]} />
+                            ))}
+                        </Bar>
+                    </BarChart>
+                </div>
+                
+            </div> */}
+
+      <div className="lg:flex">
+        <div className="w-1/2 mt-10">
+          <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Bar
+              dataKey="uv"
+              fill="#8884d8"
+              shape={<TriangleBar />}
+              label={{ position: "top" }}
+            >
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+              ))}
+            </Bar>
+          </BarChart>
+        </div>
+        <div className="w-1/2">
+          <PieChart width={400} height={400}>
+            <Pie
+              data={datas}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={80}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {datas.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+            <Legend></Legend>
+          </PieChart>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminHome;
